44069d08d7c23c804c068f1b47924ea9123d949c
Tom Nguyen <tomtnguyen88@gmail.com>
2023-07-06 14:59:27 -0700

    adds folder upload and render directories functionality

--- src/app/neo/page.tsx
+++ src/app/neo/page.tsx
@@ -2,8 +2,10 @@ import Link from "next/link";
 'use client';
 import axios from 'axios';
 import JSZip from "jszip";
+import React, { useState } from 'react';
 
 export default function Neo() {
+  const [fileStructure, setFileStructure] = useState<null | Array<FileItem> | []>(null);
 
   // //FORM DATA CREATION FUNCTION
   // function createFormData(fileElement: any) {
@@ -39,32 +41,85 @@ export default function Neo() {
     const filesInput: any = document.getElementById('fileInput');
     const files: any = filesInput.files
     const zip = new JSZip()
-    for(const file of files) {
-      console.log(file);
+
+
+// CREATE ARRAY OF FILE ITEMS, FILTER PRIORITY
+    const newFileStructure: Array<FileItem> = [];
+    for (const file of files) {
+      if (!file.webkitRelativePath.includes('node_modules') && 
+      !file.webkitRelativePath.includes('webpack') &&
+      !file.webkitRelativePath.includes('.next') &&
+      !file.webkitRelativePath.includes('config') &&
+      !file.webkitRelativePath.includes('.git') &&
+      !file.type.includes('image')
+      ) {
+        // SEPARATE FOLDERS FROM FILES
+        const filePath = file.webkitRelativePath;
+        const filePathParts = filePath.split('/');
+        const fileName = filePathParts.pop() as string;
+        let currentFolder = newFileStructure;
+
+        for (const folderName of filePathParts) {
+          const existingFolder = currentFolder.find((item) => item.name === folderName);
+
+          if (existingFolder && existingFolder.type === 'folder') {
+            currentFolder = existingFolder.files as FileItem[];
+          } else {
+            const newFolder: FileItem = {
+              name: folderName,
+              type: 'folder',
+              size: 0,
+              lastModified: 0,
+              files: [],
+            };
+            currentFolder.push(newFolder);
+            currentFolder = newFolder.files as FileItem[];
+          }
+        }
+
+        currentFolder.push({
+          name: fileName,
+          type: 'file',
+          size: file.size,
+          lastModified: file.lastModified,
+        });
+
+        console.log(file);
+      }
     }
+
+    setFileStructure(newFileStructure);
   }
+
 
   return (
     <>
     <div id="content" className="bg-gray-300 rounded-3xl">
       <div id="app-header" className="flex justify-between">
         <p className="text-3xl text-black ml-10">Dashboard</p>
-        <input className="bg-black rounded-md p-2 mr-10" id="fileInput" type="file" name="directory" webkitdirectory="true" onChange={createZip} ></input>
+        <input 
+          className="bg-black rounded-md p-2 mr-10 text-white" 
+          id="fileInput" 
+          type="file" 
+          name="directory" 
+          webkitdirectory="true" 
+          onChange={createZip} 
+        ></input>
       </div>
       <div id="app-header_line" className="bg-black rounded-xl"></div>
       <div id="app-body" className="flex">
         <div id="app-sidebar" className="flex flex-col ml-20 text-black">
-          File.js
-          <button className="bg-black rounded-md p-2 text-white">
+          {fileStructure && <FileItem item={fileStructure} />}
+          {/* <button className="bg-black rounded-md p-2 text-white">
             Add Folder
-          </button>
+          </button> */}
         </div>
         <div id="app-body_line" className="bg-black"></div>
         <div
           id="app-main"
           className="flex flex-col justify-center text-black"
         >
-          File.js
+
           <button className="bg-black rounded-md p-2 text-white">
             Generate
           </button>
@@ -73,6 +128,40 @@ export default function Neo() {
     </div>
     </>
   );
+
+  type FileItem = {
+    name: string;
+    type: 'file' | 'folder';
+    size: number;
+    lastModified: number;
+    files?: FileItem[];
+  };
+
+  function FileItem({
+    item, onClick,
+  }: {
+    item: FileItem[];
+    onClick: (folderName: string) => void;
+  }) {
+    const handleClick = (folderName: string) => {
+      onClick(folderName);
+    };
+
+    return (
+      <ul>
+        {item.map((file) => (
+          <li key={file.name}>
+            {file.type === 'file' ? (
+              <span>{'__' + file.name}</span>
+            ) : (
+              <strong onClick={() => handleClick(file.name)}>{'/' + file.name}</strong>
+          )}
+          {file.files && <FileItem item={file.files} onClick={onClick} />}
+        </li>
+      ))}
+    </ul>
+  );
+}
 }
 
 //ORIGINAL TEST CODE
@@ -98,4 +187,3 @@ New modules
 1. axios - for streamlining server requests
 2. jszip - for zipping uploads
 */
-
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="flex justify-around items-center" id="content">
+    <div className="flex justify-around items-center text-white" id="content">
       <div>
         <p className="text-3xl">Next</p>
         <p className="text-3xl">Engine</p>
