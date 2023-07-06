b337a9784fc40d734549bf7676501f4a976f23d9
Tom Nguyen <tomtnguyen88@gmail.com>
2023-07-06 15:50:47 -0700

    resolve changes, now renders directory and graphics

--- src/app/neo/page.tsx
+++ src/app/neo/page.tsx
@@ -1,9 +1,9 @@
-'use client';
+// 'use client';
 import App from "./app";
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
 export default function Neo() {
-  const [fileStructure, setFileStructure] = useState<null | Array<FileItem> | []>(null);
+  // const [fileStructure, setFileStructure] = useState<null | Array<FileItem> | []>(null);
 
   // //FORM DATA CREATION FUNCTION
   // function createFormData(fileElement: any) {
@@ -35,59 +35,13 @@ export default function Neo() {
 
 
   //FILE ZIP FUNCTION TO RUN ONCHANGE
-  function createZip () {
-    const filesInput: any = document.getElementById('fileInput');
-    const files: any = filesInput.files
-    const zip = new JSZip()
+  // function createZip () {
+  //   const filesInput: any = document.getElementById('fileInput');
+  //   const files: any = filesInput.files
+  //   const zip = new JSZip()
 
 
-// CREATE ARRAY OF FILE ITEMS, FILTER PRIORITY
-    const newFileStructure: Array<FileItem> = [];
-    for (const file of files) {
-      if (!file.webkitRelativePath.includes('node_modules') && 
-      !file.webkitRelativePath.includes('webpack') &&
-      !file.webkitRelativePath.includes('.next') &&
-      !file.webkitRelativePath.includes('config') &&
-      !file.webkitRelativePath.includes('.git') &&
-      !file.type.includes('image')
-      ) {
-        // SEPARATE FOLDERS FROM FILES
-        const filePath = file.webkitRelativePath;
-        const filePathParts = filePath.split('/');
-        const fileName = filePathParts.pop() as string;
-        let currentFolder = newFileStructure;
 
-        for (const folderName of filePathParts) {
-          const existingFolder = currentFolder.find((item) => item.name === folderName);
-
-          if (existingFolder && existingFolder.type === 'folder') {
-            currentFolder = existingFolder.files as FileItem[];
-          } else {
-            const newFolder: FileItem = {
-              name: folderName,
-              type: 'folder',
-              size: 0,
-              lastModified: 0,
-              files: [],
-            };
-            currentFolder.push(newFolder);
-            currentFolder = newFolder.files as FileItem[];
-          }
-        }
-
-        currentFolder.push({
-          name: fileName,
-          type: 'file',
-          size: file.size,
-          lastModified: file.lastModified,
-        });
-
-        console.log(file);
-      }
-    }
-
-    setFileStructure(newFileStructure);
-  }
 
 
   return (
@@ -96,73 +50,42 @@ export default function Neo() {
     </div>
   );
 
-  type FileItem = {
-    name: string;
-    type: 'file' | 'folder';
-    size: number;
-    lastModified: number;
-    files?: FileItem[];
-  };
+  
 
-  function FileItem({
-    item, onClick,
-  }: {
-    item: FileItem[];
-    onClick: (folderName: string) => void;
-  }) {
-    const handleClick = (folderName: string) => {
-      onClick(folderName);
-    };
+  // type FileItem = {
+  //   name: string;
+  //   type: 'file' | 'folder';
+  //   size: number;
+  //   lastModified: number;
+  //   files?: FileItem[];
+  // };
 
-    return (
-      <ul>
-        {item.map((file) => (
-          <li key={file.name}>
-            {file.type === 'file' ? (
-              <span>{'__' + file.name}</span>
-            ) : (
-              <strong onClick={() => handleClick(file.name)}>{'/' + file.name}</strong>
-          )}
-          {file.files && <FileItem item={file.files} onClick={onClick} />}
-        </li>
-      ))}
-    </ul>
-  );
+//   function FileItem({
+//     item, onClick,
+//   }: {
+//     item: FileItem[];
+//     onClick: (folderName: string) => void;
+//   }) {
+//     const handleClick = (folderName: string) => {
+//       onClick(folderName);
+//     };
 
-  type FileItem = {
-    name: string;
-    type: 'file' | 'folder';
-    size: number;
-    lastModified: number;
-    files?: FileItem[];
-  };
-
-  function FileItem({
-    item, onClick,
-  }: {
-    item: FileItem[];
-    onClick: (folderName: string) => void;
-  }) {
-    const handleClick = (folderName: string) => {
-      onClick(folderName);
-    };
-
-    return (
-      <ul>
-        {item.map((file) => (
-          <li key={file.name}>
-            {file.type === 'file' ? (
-              <span>{'__' + file.name}</span>
-            ) : (
-              <strong onClick={() => handleClick(file.name)}>{'/' + file.name}</strong>
-          )}
-          {file.files && <FileItem item={file.files} onClick={onClick} />}
-        </li>
-      ))}
-    </ul>
-  );
-}
-}
+//     return (
+//       <ul>
+//         {item.map((file) => (
+//           <li key={file.name}>
+//             {file.type === 'file' ? (
+//               <span>{'__' + file.name}</span>
+//             ) : (
+//               <strong onClick={() => handleClick(file.name)}>{'/' + file.name}</strong>
+//           )}
+//           {file.files && <FileItem item={file.files} onClick={onClick} />}
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// }
+// }
 }
 
 //ORIGINAL TEST CODE
@@ -175,9 +98,9 @@ export default function Neo() {
 </form>
 */
 
-{
+// {
   /* <input id="fileInput" type="file" onChange={ test } webkitdirectory="true" multiple></input> */
-}
+// }
 
 //NOTES
 /*
