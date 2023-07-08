298261bbe1a51b6df70532e5b14aa4f3ec6ecec4
Donald Twiford <donaldptwiford@gmail.com>
2023-07-08 09:34:06 -0700

    beark apart post request into important chunks

--- src/app/api/fileUpload/route.ts
+++ src/app/api/fileUpload/route.ts
@@ -5,6 +5,7 @@ import { createEdgeRouter } from "next-connect";
 import fs from 'fs';
 import decompress from 'decompress';
 import * as fsX from 'fs-extra';
+import next from 'next/types';
 
 //SETUP FOR NEXT-CONNECT ROUTER
 interface RequestContext {
@@ -17,25 +18,28 @@ interface RequestContext {
 const router = createEdgeRouter<NextRequest, RequestContext>();
 
 router
-  //FILE CLEANUP on post
-  .use(async(req, event, next) => {
+  //FILE CLEANUP
+  .post(async(req, event, next) => {
       fsX.emptyDirSync('./test/zip');
       fsX.emptyDirSync('./test/unzip');
     return next()
   })
 
-  /* POST for Zip Files */
-  .post(async(req, event, next) => {
-    const blobZip = await req.blob()
-    console.log('blob: ', await blobZip);
-    const fileBuffer: any = await blobZip.arrayBuffer()
-    const data = await new DataView(fileBuffer);
-    console.log('dataView', data)
-    fs.writeFileSync('test/zip/files.zip', data);
-    await decompress('test/zip/files.zip', 'test/unzip');
-    fsX.emptyDirSync('./test/zip');
-    return NextResponse.json('Files successfully loaded');
-  }) 
+  // //CREATE ZIP
+  // .post(async(req, event, next) => {
+  //   const blobZip = await req.blob()
+  //   const fileBuffer: any = await blobZip.arrayBuffer()
+  //   const data = new DataView(fileBuffer);
+  //   fs.writeFileSync('test/zip/files.zip', data);
+  //   return next()
+  // })
+
+  // /* UNPACK ZIP FILE */
+  // .post(async(req, event, next) => {
+  //   await decompress('test/zip/files.zip', 'test/unzip');
+  //   fsX.emptyDirSync('./test/zip');
+  //   return NextResponse.json('Files successfully loaded');
+  // }) 
 
 export async function POST(request: NextRequest, ctx: RequestContext) {
   return router.run(request, ctx);
