58fddae69126fbbd0ea3ff767f102bebe7035001
Donald Twiford <donaldptwiford@gmail.com>
2023-07-10 16:07:42 -0700

    remove supertest and node mocks https until needed, install ts-node, update babel config to ES6 and set up basic jest.config.ts file withno implemeneted additional options

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,4 @@
-module.exports = {
-  presets: [
-    ['@babel/preset-env', {targets: {node: 'current'}}],
-    '@babel/preset-typescript',
-  ],
-};
+export const presets = [
+  ['@babel/preset-env', { targets: { node: 'current' } }],
+  '@babel/preset-typescript',
+];
