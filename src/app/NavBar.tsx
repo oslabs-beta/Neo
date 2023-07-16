d810d75a0832f8a4ec8b1e309bd43cfcea77fd30
Nitesh Sunku <nsunku99@gmail.com>
2023-07-15 23:36:27 -0700

    styling and flex for dynamic media adjustments

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -4,16 +4,16 @@ import Image from 'next/image';
 export default function NavBar() {
   return (
     <nav>
-      <div className="mx-auto w-[69%]">
-        <div className="flex justify-between mx-5">
+      <div className="mx-auto w-[65%]">
+        <div className="flex flex-wrap items-center justify-between gap-x-10">
           <Image className='my-5' src="/Neo-White.png" width={220} height={220} priority={true} alt="Neo Logo" />
-          <div className="flex justify-between w-[30%] my-auto text-2xl font-light text-white">
+          <div className="flex justify-between gap-x-8 w-[30%] my-auto text-2xl font-light text-white">
             <Link id="navHome" href="/">Home</Link>
             <Link id="navApp" href="/neo">App</Link>
             <Link id="navContact" href="/contact">Contact</Link>
           </div>
         </div>
-        <div className="navline"></div>
+        {/* <div className="navline"></div> */}
       </div>
     </nav>
   )
